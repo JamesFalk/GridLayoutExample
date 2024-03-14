4a30af0da5598aa4d449caefd5522f7be7248899
James Falk <60024639+JamesFalk@users.noreply.github.com>
2024-03-14 12:13:17 -0600

    Update GridLayoutExample.py

--- GridLayoutExample.py
+++ GridLayoutExample.py
@@ -1,56 +1,85 @@
-from tkinter import Tk, Frame, Canvas, Label
+from datetime import datetime
+from tkinter import Tk, Frame, Canvas, Label, BooleanVar, Checkbutton
+from tkinter import Entry, StringVar, ttk, Button
+from PIL import Image, ImageTk
+from pygame import mixer
+
+# Initializing the Sound Mixer
+mixer.init()
 
 # Creating the Main Window
 master = Tk(); master.title("Grid Layout Example")
 master.geometry("800x500+200+200")
 
-# Create the master_grid
+# Create the master_grid with 10 rows and 10 columns
 mstr_grid = Frame(master, width=800, height=500)
 mstr_grid.grid()
-
 for i in range(10):  # Create 10 rows
     mstr_grid.grid_rowconfigure(i, weight=1, minsize=50)
 for i in range(10):  # Create 10 columns
     mstr_grid.grid_columnconfigure(i, weight=1, minsize=80)
-
 mstr_grid.grid_propagate(False)
 
-# Create a container box_grid
-box_grid = Frame(mstr_grid, width=300, height=200)
+### Example Widgets: ###
+# 1. Static Label
+lbl_1 = Label(mstr_grid, text="Here is a clock label:")
+lbl_1.grid(row=0, column=0, columnspan=2, sticky='nw')
+
+# 2. Clock_Label
+def time():
+    new_date = datetime.now().strftime("%m-%d-%Y")
+    new_time = datetime.now().strftime("%H:%M:%S %p")
+    date_time = (f'Date is ' + new_date + '\nTime is ' + new_time)
+    lbl_Clock_Label.config(text=date_time)
+    lbl_Clock_Label.after(1000, time)
+lbl_Clock_Label = Label(mstr_grid, font=('calibri', 12, 'bold'))
+lbl_Clock_Label.grid(row=1, column=1, columnspan=2, sticky='nw'); time()
+
+# 3. Checkbox
+checkvar = BooleanVar()
+checkbox = Checkbutton(mstr_grid, text="Check This Out!", variable=checkvar)
+checkbox.grid(row=2, column=2, columnspan=2, sticky='nw')
+
+# 4. Image on Canvas inside a box_grid on mstr_grid
+# Create a box_grid frame on mstr_grid
+box_grid = Frame(mstr_grid, width=300, height=300)
 box_grid.grid_propagate(False)
-box_grid.grid(row=3, column=3, columnspan=3, rowspan=3, sticky='nw')
+box_grid.grid(row=3, column=3, rowspan=4, columnspan=4, sticky='nw')
 # Create the Canvas
-canvas = Canvas(box_grid, bg="blue"); canvas.grid()
+canvas = Canvas(box_grid); canvas.grid()
+image = Image.open("GitHubProfile.jpg")
+image = image.resize((250, 200), Image.LANCZOS)
+img = ImageTk.PhotoImage(image)
+canvas.create_image(0, 0, anchor="nw", image=img)
 
-# Example Labels
-lbl_1 = Label(mstr_grid, text="label 1")
-lbl_1.grid(row=0, column=0, sticky='nw')
+# 5. Entrybox
+entered_text = StringVar()
+entrybox = Entry(mstr_grid, textvariable=entered_text)
+entrybox.grid(row=7, column=7, columnspan=3, sticky='nw')
 
-lbl_2 = Label(mstr_grid, text="label 2")
-lbl_2.grid(row=1, column=1, sticky='nw')
+# 6. Combobox
+optn_select = StringVar()
+size_ddbox = ttk.Combobox(mstr_grid, width=15, textvariable=optn_select)
+size_ddbox['values'] = ('Small', 'Medium', 'Large', 'Extra Large')
+size_ddbox.grid(row=8, column=8, sticky='nw'); size_ddbox.current(1)
 
-lbl_3 = Label(mstr_grid, text="label 3")
-lbl_3.grid(row=2, column=2, sticky='nw')
+# 7. Button
+btn_button = Button(mstr_grid, text="Press Button", bg="green", fg="orange")
+btn_button.grid(row=9, column=9, sticky='news')
+# Making A Button Function that Plays Sound if Checkbox is Checked
+def btn_button_click(yo):
+    if checkvar.get(): mixer.music.load('YoYoYoBuddy.wav'); mixer.music.play()
+# Triggering The Button Event
+btn_button.bind("<Button-1>", btn_button_click)
+# note - there is a big array of button bindings to choose from such as
+# <Enter> Hover In, <Leave> Hover Out <Button-3> Right Mouse
 
-lbl_4 = Label(mstr_grid, text="label 4")
-lbl_4.grid(row=3, column=3, sticky='nw')
-
-lbl_5 = Label(mstr_grid, text="label 5")
-lbl_5.grid(row=4, column=4, sticky='nw')
-
-lbl_6 = Label(mstr_grid, text="label 6")
-lbl_6.grid(row=5, column=5, sticky='nw')
-
-lbl_7 = Label(mstr_grid, text="label 7")
-lbl_7.grid(row=6, column=6, sticky='nw')
-
-lbl_8 = Label(mstr_grid, text="label 8")
-lbl_8.grid(row=7, column=7, sticky='nw')
-
-lbl_9 = Label(mstr_grid, text="label 9")
-lbl_9.grid(row=8, column=8, sticky='nw')
-
-lbl_10 = Label(mstr_grid, text="label 10")
-lbl_10.grid(row=9, column=9, sticky='nw')
+# 8. Making a display_label that Displays text entered in the Entrybox
+dsply_lbl = Label(mstr_grid, textvariable=entered_text, font=('Courier', 12, 'italic'))
+dsply_lbl.grid(row=8, column=1, columnspan=5, sticky='nw')
+def update_size(hi):
+    size_mtrx = [8, 12, 18, 26]; size = size_mtrx[size_ddbox.current()]
+    dsply_lbl.config(font=('Courier', size, 'italic'))
+size_ddbox.bind('<<ComboboxSelected>>', update_size)
 
 master.mainloop()
